a61743035f509233463cbfff5e117430dc71c26f
bogus <boguslawski.m.j@gmail.com>
2026-01-10 12:07:57 +0000

    Added tests for model and ppo loss

--- rl_agents/tests/unit/network/test_backbone.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-import torch as T
-
-
-folder_path = Path(__file__).parent.absolute()
-states_path = os.path.join(folder_path, "data", "states.pt")
-states = T.load(states_path)
-print(states)
